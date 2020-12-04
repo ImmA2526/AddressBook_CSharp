dc03b3a3e5cbb70a2511ba5b9ce56d4d092b2714
Avenger2627 <imraninfo97@gmail.com>
2020-12-04 17:27:36 +0530

    [Imran Shaikh] Add: Ability To create New Contact in AddressBook

---
+++ PersonalDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address
+{
+    class PersonalDetail
+    {
+        private string FirstName;
+        private string LastName;
+        private string State;
+        private string Address;
+        private int Pincode;
+        private int MobileNumber;
+        public PersonalDetail(string FirstName, string LastName, int Pincode, int MobileNumber, string State, string Address)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.State = State;
+            this.Address = Address;
+            this.Pincode = Pincode;
+            this.MobileNumber = MobileNumber;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nFirst Name: \t " +FirstName+ "\n Last Name: \t "+ LastName+ "\n State: \t " + State + "\n Address: \t " + Address+"\n PinCode: \t " +Pincode+ "\n Mob Number:\t  "+MobileNumber);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace Address
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome to AddressBook**********");
+            PersonalDetail Detail = new PersonalDetail("","",0 ,0,"","");
         }
-    }
+    } 
 }
